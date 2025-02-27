3c817c360ea3b33109e13de57c1efff698e99083
Chandan Mehto <mehtochandan78@gmail.com>
2025-02-27 22:49:57 +0530

    Learned about utils discussion

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 // whenever you talk with database always use async await and try -catch.
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
-
-
-
+.then(()=>{
+    app.listen(process.env.PORT  || 8000, ()=>{
+        console.log(`Server is runnint  at port : ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log(`MongoDB connection failed  !!!${err}`);
+    
+})
 
 
 
